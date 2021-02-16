3126ca54061e1aef4f9a2b42f657c1ef5290c99a
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-02-16 20:42:06 +0100

    Modify cards

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,24 @@
 import './Card.css'
+import { useState } from 'react'
 
-function Card({ name, species, image }) {
+function Card({ name, species, image, gender, status, location }) {
+  const [isDetailsShown, setDetailsShown] = useState(false)
+
   return (
     <>
       <section className="Card">
         {name} {species === 'Human' ? '👤' : '👽'}
-        <img className="characterImg" src={image} alt="" />
+        <img className="Card__characterImg" src={image} alt="" />
+        <button onClick={() => setDetailsShown(!isDetailsShown)}>
+          {isDetailsShown ? 'Hide' : 'Show more'}
+        </button>
+        <section hidden={!isDetailsShown}>
+          <ul>
+            <li> Gender: {gender} </li>
+            <li> Status: {status} </li>
+            <li> Origin: {location} </li>
+          </ul>
+        </section>
       </section>
     </>
   )
