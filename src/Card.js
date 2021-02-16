082d83c7b1201a195c1f102f594fe8f0b91cdd6c
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-02-16 12:54:37 +0100

    Add icon for species

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,12 @@ import './Card.css'
 
 function Card({ name, species, image }) {
   return (
-    <section className="Card">
-      <h1>{name} </h1> Species: {species}
-      <img className="characterImg" src={image} alt="" />
-    </section>
+    <>
+      <section className="Card">
+        {name} {species === 'Human' ? '👤' : '👽'}
+        <img className="characterImg" src={image} alt="" />
+      </section>
+    </>
   )
 }
 
