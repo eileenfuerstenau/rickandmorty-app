dfc9ee131c6a1dec5046f35ee632c610591e41e9
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-02-18 16:10:41 +0100

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Card from './Card'
+import Header from './Header'
 import './App.css'
 import React, { useState, useEffect } from 'react'
 
 function App() {
   const [characters, setCharacters] = useState([])
+
   useEffect(() => {
     getAllCharacters()
-  })
+  }, [])
 
   function getAllCharacters(url = 'https://rickandmortyapi.com/api/character') {
     fetch(url)
@@ -19,19 +21,22 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {characters.map(item => (
-        <Card
-          key={item.id}
-          name={item.name}
-          species={item.species}
-          image={item.image}
-          gender={item.gender}
-          status={item.status}
-          location={item.origin.name}
-        />
-      ))}
-    </div>
+    <>
+      <Header title="Rick and Morty" />
+      <div className="App">
+        {characters.map(item => (
+          <Card
+            key={item.id}
+            name={item.name}
+            species={item.species}
+            image={item.image}
+            gender={item.gender}
+            status={item.status}
+            location={item.origin.name}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
