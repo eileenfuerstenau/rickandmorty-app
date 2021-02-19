6b522ad5c031c2da61efd8917989c019144d0b89
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-02-19 15:06:55 +0100

    Implement search bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import getAllCharacters from '../../services/getAllCharacters'
 
 function App() {
   const [characters, setCharacters] = useState([])
+  const [userInput, setUserInput] = useState('')
 
   useEffect(() => {
     getAllCharacters({
@@ -16,17 +17,27 @@ function App() {
   return (
     <>
       <div className="App">
-        {characters.map(item => (
-          <Card
-            key={item.id}
-            name={item.name}
-            species={item.species}
-            image={item.image}
-            gender={item.gender}
-            status={item.status}
-            location={item.origin.name}
-          />
-        ))}
+        <input
+          value={userInput}
+          onChange={event => setUserInput(event.target.value)}
+          placeholder="Search character..."
+        />
+
+        {characters
+          .filter(character =>
+            character.name.toLowerCase().includes(userInput.toLowerCase())
+          )
+          .map(item => (
+            <Card
+              key={item.id}
+              name={item.name}
+              species={item.species}
+              image={item.image}
+              gender={item.gender}
+              status={item.status}
+              location={item.origin.name}
+            />
+          ))}
       </div>
     </>
   )
