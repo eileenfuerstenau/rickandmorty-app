b9143bcce10e8fc1d35a44535c8ed1e24d29ba68
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-02-19 14:43:12 +0100

    Implement scroll snap

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,18 +6,54 @@ function Card({ name, species, image, gender, status, location }) {
 
   return (
     <>
+      <section className="scroller">
+        <div class="box">
+          {name} {species === 'Human' ? '👤' : '👽'}
+          <br /> &rarr;
+        </div>
+        <div class="box">
+          <img className="Card__characterImg" src={image} alt="" />
+        </div>
+        <div class="box">
+          <button onClick={() => setDetailsShown(!isDetailsShown)}>
+            {isDetailsShown ? 'Hide' : 'Show more'}
+          </button>
+          <section hidden={!isDetailsShown}>
+            <ul>
+              <li> Gender: {gender} </li>
+              <li> Status: {status} </li>
+              <li> Origin: {location} </li>
+            </ul>
+          </section>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Card
+
+/* 
+import './Card.css'
+import { useState } from 'react'
+
+function Card({ name, species, image, gender, status, location }) {
+  const [isDetailsShown, setDetailsShown] = useState(false)
+
+  return (
+    <>
       <section className="Card">
-        {name} {species === 'Human' ? '👤' : '👽'}
-        <img className="Card__characterImg" src={image} alt="" />
-        <button onClick={() => setDetailsShown(!isDetailsShown)}>
-          {isDetailsShown ? 'Hide' : 'Show more'}
-        </button>
-        <section hidden={!isDetailsShown}>
-          <ul>
-            <li> Gender: {gender} </li>
-            <li> Status: {status} </li>
-            <li> Origin: {location} </li>
-          </ul>
+          {name} {species === 'Human' ? '👤' : '👽'}
+          <img className="Card__characterImg" src={image} alt="" />
+          <button onClick={() => setDetailsShown(!isDetailsShown)}>
+            {isDetailsShown ? 'Hide' : 'Show more'}
+          </button>
+          <section hidden={!isDetailsShown}>
+            <ul>
+              <li> Gender: {gender} </li>
+              <li> Status: {status} </li>
+              <li> Origin: {location} </li>
+            </ul>
         </section>
       </section>
     </>
@@ -25,3 +61,4 @@ function Card({ name, species, image, gender, status, location }) {
 }
 
 export default Card
+*/
